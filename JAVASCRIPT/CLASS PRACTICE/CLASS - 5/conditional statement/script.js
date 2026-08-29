793f1fe97471f58e7a5baea17c8afbffc4defe6c
MEssadev7 <muhammad.essa9078@gmail.com>
2026-08-29 15:18:24 +0500

    condetional statement

---
+++ JAVASCRIPT/CLASS PRACTICE/CLASS - 5/conditional statement/script.js
@@ -0,0 +1,13 @@
+// if ,else , else if,
+
+// reserve words we can't use it as a variable
+
+// alternative of if else
+// let result = age >= 18 ? "your eligible" : "your not eligiable";
+
+let age = Number(prompt("enter your age", 18));
+if (age >= 18) {
+  document.write("your eligiable");
+} else {
+  document.write("your not eligiable");
+}
