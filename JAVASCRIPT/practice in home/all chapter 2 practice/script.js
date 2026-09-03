a4dba00e8be20ba7a79588a022c347906ad78d99
MEssadev7 <muhammad.essa9078@gmail.com>
2026-09-03 21:56:32 +0500

    some  practice

---
+++ JAVASCRIPT/practice in home/all chapter 2 practice/script.js
@@ -0,0 +1,81 @@
+// if else condition
+
+/*
+let age = prompt("what is your age");
+if (age >= 10 && age <= 18) {
+  document.write("you can go with your friends for program");
+} else {
+  document.write("your age is above 18 you have to go with your senior friend");
+}
+
+
+
+.
+.
+.
+.
+.
+.
+.
+.
+.  
+  */
+
+// awitch statement
+// let age = Number(prompt("what is your age"));
+// switch (age) {
+//   case 13:
+//     document.write("you don't think of driving");
+//     break;
+//   case 14:
+//     document.write("you also don't think of driving");
+//     break;
+//   case 15:
+//     document.write("you  think of driving");
+//     break;
+//   case 16:
+//     document.write("you litle bit more  think of driving");
+//     break;
+//   case 17:
+//     document.write("you also try to undersatand how to drive ");
+//     break;
+//   case 18:
+//     document.write("you have to be able to drive ");
+//     break;
+//   default:
+//     document.write("your age is not special for drving");
+// }
+
+/*
+.
+.
+.
+.
+.
+.
+.
+*/
+// find a number which is diviseble by 2 and 3
+// let num = Number(
+//   prompt(
+//     "enter number we check it that it is divisible by 2 and 3 we inform you",
+//   ),
+// );
+// if (num % 2 == 0 && num % 3 == 0) {
+//   document.write("your number is divisible by 2 and 3");
+// } else {
+//   document.write("your number is not divisible by 2 and 3");
+// }
+
+/*
+.
+.
+.
+.
+.
+*/
+// use trnory operetor to find correct age of driving
+// let age = 12;  //you can'tdrive because age is less than 18
+let age = 20; //you can drive because age is greater than 18
+let a = age > 18 ? "you can drive " : "you can't drive";
+document.write(a);
