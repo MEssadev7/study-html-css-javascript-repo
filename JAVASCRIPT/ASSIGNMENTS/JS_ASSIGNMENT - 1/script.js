fed30f50dd392cfbd5b7e103b8372d8d17d55606
MEssadev7 <muhammad.essa9078@gmail.com>
2026-09-14 23:09:23 +0500

    class work add and assgnment

---
+++ JAVASCRIPT/ASSIGNMENTS/JS_ASSIGNMENT - 1/script.js
@@ -0,0 +1,49 @@
+// step : 1
+// link the html file with js file
+//  <script src="script.js"></script>
+
+// step : 2
+// write hello world by alert
+alert("hello world");
+
+//step : 3
+// write hello world by console
+console.log("hello world");
+
+// step:4
+// make variable by (var , let , const)
+var studentname = "MUhammad ESSA";
+let cursename = "modern web development";
+const batch_number = 6;
+
+// step : 5
+// print the variable by console
+console.log("student name is: " + studentname);
+console.log("course name is : " + cursename);
+console.log("batch number is : " + batch_number);
+
+// step : 6
+// webpage per variable show karna hai
+document.write(
+  "welcome " +
+    studentname +
+    " to " +
+    cursename +
+    " batch number is " +
+    batch_number,
+);
+
+// practice portion
+// step : 7
+// change the value of let variable
+let course = "HTML";
+
+course = "JavaScript";
+
+console.log(course);
+
+// step : 8
+// change the value of const variable
+const batch = 12;
+batch = 13;
+console.log(batch);
